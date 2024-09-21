c9a1989d4596bb29615d3baae6338dcb72e129d0
anupam2 <anupam.udupi@gmail.com>
2024-09-22 04:55:45 +0530

    header color chnaged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,15 @@ import MainContainer from "./components/MainContainer";
 export default function Home() {
   return (
     <>
-     <AppBar>
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Real-Time Data Visualization
-        </Typography>
-      </Toolbar>
-    </AppBar>
+      <AppBar style={{ background: "radial-gradient(#491f79, #491f79)" }}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Real-Time Data Visualization
+          </Typography>
+        </Toolbar>
+      </AppBar>
 
-   <MainContainer/>
-   </>
+      <MainContainer />
+    </>
   );
 }
